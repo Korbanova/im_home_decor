c9e0b3af2f9142d2951442feaf28f9bbec4f117a
Ekaterina Savoskina <korbanova20@gmail.com>
2023-08-16 12:09:48 +0300

    init

---
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,49 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CategoryType} from "../../../../types/category.type";
+import {AuthService} from "../../../core/auth/auth.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  isLogged: boolean = false
+
+  @Input() categories: CategoryWithTypeType[] = [];
+
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar,
+              private router: Router) {
+    this.isLogged = this.authService.getIsLoggedIn();
+  }
+
+  ngOnInit() {
+    this.authService.isLogged$.subscribe((isLoggedIn: boolean) => {
+      this.isLogged = isLoggedIn;
+    })
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: (data: DefaultResponseType) => {
+        this.doLogout();
+      },
+      error: (errorResponse: HttpErrorResponse) => {
+        this.doLogout();
+      }
+    })
+  }
+
+  doLogout(): void {
+    this.authService.removeTokens();
+    this.authService.userId = null;
+    this._snackBar.open('Вы вышли из сиситемы');
+    this.router.navigate(['/']);
+  }
+}
---
+++ frontend/src/app/views/product/datail/datail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datail',
+  templateUrl: './datail.component.html',
+  styleUrls: ['./datail.component.scss']
+})
+export class DatailComponent {
+
+}
