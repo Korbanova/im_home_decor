3f69f5d5cd2cb8e5c06f671e54e0472a5d2bed84
Ekaterina Savoskina <korbanova20@gmail.com>
2023-09-06 23:02:44 +0300

    change environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   api: "https://ws-example.site/api/",
-  serverStaticPath: 'https://ws-example.site/products/'
+  serverStaticPath: 'https://ws-example.site/images/products/'
 };
